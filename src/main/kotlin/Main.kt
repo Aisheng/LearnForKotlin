cc88b16019f1d7e7f82aa3009df8351c851d9e4f
Aisheng <Lidaisheng7142@163.com>
2018-03-03 22:56:00 +0800

    添加RxJava

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ object Main {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        println("hello world")
+        RxJava.thread()
+        readLine()
     }
-
 }
---
+++ src/main/kotlin/RxJava.kt
@@ -0,0 +1,71 @@
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Flowable
+import io.reactivex.Scheduler
+import io.reactivex.schedulers.Schedulers
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+object RxJava {
+
+    fun today(): Flowable<String> {
+        return Flowable.create({
+            it.onNext("Today")
+            it.onNext("is")
+            it.onNext(SimpleDateFormat("yyyy-MM-dd").format(Date()))
+            it.onNext("a nice day")
+        }, BackpressureStrategy.BUFFER)
+    }
+
+    fun numbers(): Flowable<Int> {
+        return Flowable.range(1, 10)
+    }
+
+    fun flatMap() {
+        today().flatMap { Flowable.just(it.length) }
+                .map { it.plus(100) }
+                .doOnSubscribe { println("doOnSubscribe") }
+                .subscribe {
+                    println(it)
+                }
+    }
+
+    fun interval(): Flowable<Long> {
+        return Flowable.intervalRange(1, 10, 0, 300, TimeUnit.MILLISECONDS)
+    }
+
+    fun interval2(): Flowable<Long> {
+        return Flowable.intervalRange(100, 10, 1000, 300, TimeUnit.MILLISECONDS)
+    }
+
+    fun merge() {
+        Flowable.merge(interval(), interval2())
+                .map {
+                    if (it == 104L) {
+                        return@map Throwable()
+                    } else {
+                        return@map it
+                    }
+                }
+                .doOnComplete { println("doOnComplete") }
+                .onErrorReturn { 99 }
+                .subscribe { println(it) }
+    }
+
+    fun thread() {
+        Flowable.create<Int>({
+            println(Thread.currentThread().name)
+            it.onNext(1)
+            it.onComplete()
+        }, BackpressureStrategy.BUFFER)
+                .subscribeOn(Schedulers.newThread())
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.newThread())
+                .doOnNext { println(Thread.currentThread().name) }
+                .observeOn(Schedulers.computation())
+                .observeOn(Schedulers.io())
+                .subscribe { println(Thread.currentThread().name) }
+    }
+
+
+}
